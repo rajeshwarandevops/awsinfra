1f9f2da810bb0768c3800384fa3b03a37d076327
Rajeshwaran <112589969+rajeshwarandevops@users.noreply.github.com>
2023-03-09 04:34:28 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "devinstance" {
   ami           = "ami-0ff7189cf12ed8eb6" # us-east-1
   instance_type = "t2.micro"
   tags = {
