bfd782f14a7ed587e898ea8f7eed80979d789c4e
Rajeshwaran <112589969+rajeshwarandevops@users.noreply.github.com>
2023-03-09 05:27:13 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_instance" "devinstance" {
   ami           = "ami-0ff7189cf12ed8eb6" # us-east-1
   instance_type = "t2.micro"
+  encrypted     = true
+    ebs_optimized = true
+    monitoring = true  
+    iam_instance_profile = "devops"
+
   tags = {
       Name = "dev-Instance"
       Managedby = "Terraform"
