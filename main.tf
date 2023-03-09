22f65ca81142d4f4e48e19b07aef37f02ebdf8db
Rajeshwaran <112589969+rajeshwarandevops@users.noreply.github.com>
2023-03-09 05:30:39 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "devinstance" {
   ami           = "ami-0ff7189cf12ed8eb6" # us-east-1
   instance_type = "t2.micro"
-  encrypted     = true
-    ebs_optimized = true
+   ebs_optimized = true
     monitoring = true  
     iam_instance_profile = "devops"
 
